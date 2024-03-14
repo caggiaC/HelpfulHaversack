c6347a52a006aca08e124e2dfe0ca732c6820d30
caggiaC <ctcaggia.chris@gmail.com>
2024-03-13 20:50:26 -0400

    Encrypter class is no a singleon

---
+++ Services.ContainerAPI/Data/Encrypter.cs
@@ -0,0 +1,24 @@
+﻿namespace HelpfulHaversack.Services.ContainerAPI.Data
+{
+    public class Encrypter
+    {
+        private readonly Encrypter _instance = new();
+
+        private Encrypter()
+        {
+            _instance = new Encrypter();
+        }
+
+        private Encrypter getInstance { get { return _instance; } }
+
+        public string Encrypt(string input)
+        {
+            return string.Empty;
+        }
+
+        public string Decrypt(string input)
+        {
+            return string.Empty;
+        }
+    }
+}
