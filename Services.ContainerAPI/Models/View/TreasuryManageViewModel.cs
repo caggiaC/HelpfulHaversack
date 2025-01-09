f79bdfffde587d1f8f6e007e1d1e5dfba9a801cb
caggiaC <ctcaggia.chris@gmail.com>
2025-01-08 22:40:03 -0500

    Added View folder to the Models; added View class for the TreasuryManage view

---
+++ Services.ContainerAPI/Models/View/TreasuryManageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Services.ContainerAPI.Models.Dto;
+
+namespace HelpfulHaversack.Services.ContainerAPI.Models.View
+{
+    public class TreasuryManageViewModel
+    {
+        public TreasuryDto? Treasury { get; set; }
+
+        public List<TreasuryDto>? TreasuryList { get; set; }
+    }
+}
